e08c465f17ba4fc8df94c1a99e91adb0fe4c30a0
Skillove <167649653+Skillove@users.noreply.github.com>
2024-04-21 18:57:23 -0300

    Alterado

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ checkoutBtn.addEventListener("click", function(){
     }).join("")
 
     const message = encodeURIComponent(cartItems)
-    const phone = "67991851966"
+    const phone = "67998528012"
 
     window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
 
